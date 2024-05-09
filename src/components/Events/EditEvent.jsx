8ab613a149799e927f98d0c3cf9a2431f0a6e29c
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-05-09 15:51:09 +0200

    update

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,10 +1,18 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/Http.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams()
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events', params.id]
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id})
+  })
 
   function handleSubmit(formData) {}
 
